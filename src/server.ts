243c8b6da242ec3d86f10431ff46506c1a7b2a26
erinpaul2002 <erinpaul121102@gmail.com>
2025-10-21 16:52:17 +0530

    added minio sample

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,28 @@
 import express, { Request, Response } from 'express';
+import * as Minio from 'minio';
 
 const app = express();
 const port = `${process.env.PORT || 5000}`;
 
 app.use(express.json());
+
+const minioClient = new Minio.Client({
+  endPoint: process.env.MINIO_PRIVATE_HOST || 'localhost',
+  port: parseInt(process.env.MINIO_PRIVATE_PORT || '9000'),
+  useSSL: false,
+  accessKey: process.env.MINIO_ROOT_USER,
+  secretKey: process.env.MINIO_ROOT_PASSWORD,
+});
+
+app.get('/buckets', async (req: Request, res: Response) => {
+  try {
+    const buckets = await minioClient.listBuckets();
+    res.json(buckets);
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching buckets' });
+  }
+});
+
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
